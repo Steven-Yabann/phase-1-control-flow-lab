8b2c7cb978551df4f0079ea6903a099f8674c140
steven Yabann <stevekiptoo1@gmail.com>
2023-03-14 15:10:20 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  if (feet <= 400){
+    return 'This one is on me!'
+  }
+  else if (feet < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+  else (feet > 2500);{
+    return 'No can do.'
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  switch(city){
+    case 'NYC':
+      return 'Ok, sounds good.'
+      break;
+    case 'Pittsburgh':
+      return 'No go.'
+      break;
+
+  }
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+    default:
+      return 'Bye.'
+  }
 }
